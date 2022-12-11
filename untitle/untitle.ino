b6b275dfed70e2f8e9662f94ef35897fb7a8f025
Hiraeth-tenko <18950365798@163.com>
2022-12-11 21:43:11 +0800

    实现了暂停，继续，切歌
    再扒个简谱我就摆了

--- untitle/untitle.ino
+++ untitle/untitle.ino
@@ -1,6 +1,6 @@
-int cloudTONE[][2] = {
+char cloudTone[][2] = {
     {1, 4}, {1, 5}, {2, 1}, {0, 4}, {0, 1}, {0, 4}, {0, 6}, {1, 5}, {2, 1}, {1, 2}, {1, 5}, {2, 2}, {2, 2}, {1, 2}, {1, 7}, {1, 7}, {2, 1}, {2, 1}, {2, 1}, {1, 6}, {1, 7}, {2, 1}, {2, 1}, {1, 7}, {1, 7}, {1, 0}, {1, 0}, {1, 0}, {1, 0}, {0, 5}, {0, 2}, {0, 5}, {0, 6}, {0, 7}, {1, 0}, {0, 6}, {0, 3}, {0, 6}, {0, 7}, {1, 1}, {1, 3}, {1, 0}, {0, 5}, {0, 2}, {0, 5}, {1, 1}, {1, 2}, {1, 2}, {1, 4}, {1, 5}, {2, 1}, {2, 1}, {2, 1}, {1, 5}, {2, 1}, {1, 2}, {1, 5}, {2, 2}, {2, 2}, {2, 3}, {2, 3}, {1, 4}, {1, 5}, {2, 3}, {2, 2}, {2, 1}, {2, 1}, {2, 1}, {1, 0}, {1, 0}, {0, 6}, {1, 2}, {1, 5}, {0, 6}, {1, 2}, {1, 7}, {0, 4}, {0, 1}, {0, 4}, {0, 5}, {0, 6}, {1, 0}, {0, 5}, {0, 5}, {1, 1}, {1, 5}, {1, 5}, {0, 1}, {0, 5}, {1, 1}, {1, 4}, {1, 5}, {1, 1}, {1, 4}, {1, 5}, {1, 1}, {0, 5}, {0, 1}, {0, 5}, {1, 0}, {2, 1}, {2, 1}, {2, 1}, {0, 5}, {1, 1}, {1, 2}, {1, 2}, {0, 7}, {0, 7}, {1, 1}, {0, 5}, {0, 6}, {0, 6}, {0, 4}, {0, 5}, {0, 6}, {1, 1}, {1, 1}, {0, 7}, {0, 7}, {0, 6}, {0, 3}, {0, 6}, {0, 7}, {1, 1}, {0, 3}, {1, 0}, {1, 0}, {0, 2}, {0, 6}, {0, 7}, {0, 5}, {0, 5}, {0, 6}, {0, 1}, {0, 5}, {0, 6}, {0, 1}, {1, 0}, {1, 0}, {1, 0}, {0, 5}, {0, 5}, {0, 3}, {1, 0}, {0, 7}, {1, 1}, {1, 1}, {1, 1}, {0, 5}, {1, 1}, {1, 2}, {1, 2}, {1, 3}, {1, 3}, {1, 3}, {1, 2}, {1, 1}, {1, 1}, {0, 5}, {1, 1}, {1, 4}, {1, 5}, {1, 3}, {1, 3}, {1, 3}, {1, 3}, {1, 0}, {0, 4}, {0, 1}, {0, 5}, {0, 3}, {1, 0}, {0, 5}, {0, 1}, {0, 4}, {0, 5}, {0, 5}, {0, 4}, {0, 5}, {0, 4}, {0, 1}, {0, 5}, {0, 1}, {0, 4}, {1, 0}, {1, 0}, {0, 5}, {0, 5}, {0, 5}, {0, 5}};
-int cloudDUR[] = {
+char cloudDur[] = {
     2, 2, 1, 2, 2, 1, 0, 1, 1,
     2, 2, 1, 0, 2, 1, 0,
     0, 0, 0, 1, 1,
@@ -33,10 +33,30 @@ int cloudDUR[] = {
     1, 1, 1, 1, 1, 1, 1, 1,
     1, 1, 1, 1, 0, 0,
     0, 0, 0, 0};
+int cloudPer = 882;
 
 const int anPin[6] = {0, 1, 2, 3, 4, 5};
 const int tonePin = 11;
 const int LiPin[6] = {2, 3, 4, 5, 6, 7};
+int playMode = 1; // 0 播放乐曲; 1 自行演奏
+int playType = -1;
+int preType = -1;
+
+int stopFlag = 0;
+int reFlag = 0;
+int musicLen = -1, musicPer = -1;
+char (*musicTone)[2] = NULL, (*musicDur) = NULL;
+int sounds[6] = {};
+int CTONE[3][8] = {
+    {0, 131, 147, 165, 175, 196, 221, 248},
+    {0, 262, 294, 330, 350, 393, 441, 495},
+    {0, 525, 589, 661, 700, 786, 882, 990}};
+float duration[5] = {1.0, 1.0 / 2, 1.0 / 4, 1.0 / 8, 1.0 / 16};
+
+void readCodes();
+void ownInit();
+void musicInit();
+
 void setup()
 {
     // put your setup code here, to run once:
@@ -46,40 +66,15 @@ void setup()
         pinMode(LiPin[i], OUTPUT);
         digitalWrite(LiPin[i], HIGH);
     }
-
+    ownInit();
+    musicInit();
     Serial.begin(9600);
 }
-
-int playMode = 1; // 0 播放乐曲; 1 自行演奏
-int playType = -1;
-int preType = -1;
-int sounds[6] = {};
-int CTONE[3][8] = {
-    {0, 131, 147, 165, 175, 196, 221, 248},
-    {0, 262, 294, 330, 350, 393, 441, 495},
-    {0, 525, 589, 661, 700, 786, 882, 990}};
-float duration[5] = {1.0, 1.0 / 2, 1.0 / 4, 1.0 / 8, 1.0 / 16};
-
-void readCodes();
 
 void loop()
 {
     readCodes();
-    if (playMode == 2)
-    {
-        for (int i = 0; i < 6; ++i)
-        {
-            int val = analogRead(anPin[i]);
-            Serial.print("analogRead");
-            Serial.print(i);
-            Serial.print(": ");
-            Serial.println(val);
-            delay(1);
-        }
-        Serial.println("##########");
-        delay(1000);
-    }
-    else if (playMode == 1)
+    if (playMode == 1)
     {
         for (int i = 0; i < 6; ++i)
         {
@@ -97,6 +92,10 @@ void loop()
                 Serial.println(playType);
                 break;
             }
+            else
+            {
+                playType = -1;
+            }
         }
         if (preType != playType)
             noTone(tonePin);
@@ -105,47 +104,53 @@ void loop()
             tone(tonePin, CTONE[1][playType], 882);
             Serial.println("##########");
         }
-        if (playType != -1)
-        {
-            playType = -1;
-        }
     }
     else if (playMode == 0)
     {
-        int len = sizeof(cloudTONE) / sizeof(int) / 2;
-        int len2 = sizeof(cloudDUR) / sizeof(int);
-        /*Serial.print("len: ");
-        Serial.println(len);
-        Serial.print("len2: ");
-        Serial.println(len2);
-        delay(10000);*/
+    restart:
         Serial.println("Start");
         delay(1000);
-        for (int i = 0; i < len && playMode == 0; i++)
+        for (int i = 0; i < musicLen && playMode == 0;)
         {
-            int tune = CTONE[cloudTONE[i][0]][cloudTONE[i][1]];
-            float dur = cloudDUR[i];
-            /*Serial.print(i);
-            Serial.print(": tune:");
-            Serial.print(tune);
-            Serial.print(", dur:");
-            Serial.println(dur);*/
-            if (tune != 0)
+            if (!stopFlag)
             {
-                tone(tonePin, tune);
-                delay((int)(882 * dur));
-                noTone(tonePin);
-            }
-            else
-            {
-                delay(882);
+                int tune = CTONE[musicTone[i][0]][musicTone[i][1]];
+                float dur = musicDur[i];
+                if (tune != 0)
+                {
+                    tone(tonePin, tune);
+                    delay((int)(musicPer * dur));
+                    noTone(tonePin);
+                }
+                else
+                {
+                    delay(musicPer);
+                }
+                ++i;
             }
             readCodes();
+            if (reFlag)
+            {
+                reFlag = 0;
+                goto restart;
+            }
         }
         delay(5000);
     }
 }
 
+void ownInit()
+{
+    playType = preType = -1;
+}
+
+void musicInit()
+{
+    musicLen = sizeof(cloudDur) / sizeof(cloudDur[0]);
+    musicTone = cloudTone;
+    musicDur = cloudDur;
+    musicPer = cloudPer;
+}
 /*
     S -> Stop
     P -> Play
@@ -158,7 +163,6 @@ void readCodes()
     ava = Serial.available();
     if (ava > 0)
     {
-        Serial.println("t");
         int op = -1;
         op = Serial.read();
         Serial.print((char)op);
@@ -179,19 +183,76 @@ void readCodes()
                 {
                     playMode = 1;
                     Serial.println("set mode: play by own");
+                    ownInit();
                 }
                 break;
                 case '0':
                 {
                     playMode = 0;
                     Serial.println("set mode: play by program");
+                    Serial.println("Select music <<Night in the Brume>>");
+                    musicInit();
                 }
                 default:
                     break;
                 }
             }
             break;
-
+            case 's':
+            case 'S':
+            {
+                if (playMode == 0)
+                {
+                    stopFlag = 1;
+                }
+            }
+            break;
+            case 'p':
+            case 'P':
+            {
+                if (playMode == 0)
+                {
+                    stopFlag = 0;
+                }
+            }
+            break;
+            case 't':
+            case 'T':
+            {
+                int val = -1;
+                val = Serial.read();
+                Serial.println((char)val);
+                switch (val)
+                {
+                case '1':
+                {
+                    if (musicTone != cloudTone && musicDur != cloudDur)
+                    {
+                        reFlag = 1;
+                        musicTone = cloudTone;
+                        musicDur = cloudDur;
+                        musicPer = cloudPer;
+                        musicLen = sizeof(cloudDur) / sizeof(cloudDur[0]);
+                        Serial.println("Select music <<Night in the Brume>>");
+                    }
+                }
+                break;
+                case '0':
+                {
+                    if (musicTone != NULL && musicDur != NULL)
+                    {
+                        reFlag = 1;
+                        musicTone = NULL;
+                        musicDur = NULL;
+                        musicPer = 0;
+                        musicLen = NULL;
+                        Serial.println("Select music <<NULL>>");
+                    }
+                }
+                default:
+                    break;
+                }
+            }
             default:
                 break;
             }
